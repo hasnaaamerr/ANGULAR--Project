23bbec97fb321ae9b4cd2286b94a55d57c5675df
Kareem Hesham <karemmanso@gmail.com>
2023-07-01 23:12:32 +0300

    updated folder struc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './Component/home/home.component';
+import { HomeComponent } from './components/home/home.component';
+import { ExamPageComponent } from './components/exam-page/exam-page.component';
 
 const routes: Routes = [
   // { path: '', component: HomeComponent }
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'exam', component: ExamPageComponent }
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HomeComponent } from './Component/home/home.component';
-import { NavComponent } from './Component/nav/nav.component';
-import { FooterComponent } from './Component/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { ExamPageComponent } from './components/exam-page/exam-page.component';
 import { HttpClientModule } from '@angular/common/http';
 
